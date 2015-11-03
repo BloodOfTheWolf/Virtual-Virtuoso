2a30e96d6639091f6af8e71bcadd235d6a84235f
BloodOfTheWolf <kevin.v.hill@gmail.com>
2015-11-02 16:59:36 -0800

    [Menus] - Implemented first pass Main Menu - Implemented first pass Song Selection Menu

---
+++ Prototype/Assets/Scripts/ButtonTextControlScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonTextControlScript : MonoBehaviour {
+
+	//add public variable for each button we need in the song selection menu
+	public Button BackButton;
+	public Button TutorialButton;
+
+	// Use this for initialization
+	void Start () {
+	
+		TutorialButton.GetComponentInChildren<Text>().text = "Twinkle Twinkle Little Star (Tutorial)";
+		BackButton.GetComponentInChildren<Text>().text = "Back";
+	}
+
+	public void BackButtonPress()
+	{
+		Application.LoadLevel (0);
+	}
+
+	public void TutorialStart()
+	{
+		Application.LoadLevel (2);
+	}
+}
---
+++ Prototype/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	public Canvas quitMenu;
+	public Button startText;
+	public Button marketText;
+	public Button optionsText;
+	public Button exitText;
+
+	// Use this for initialization
+	void Start () {
+	
+		quitMenu = quitMenu.GetComponent<Canvas> ();
+		startText = startText.GetComponent<Button> ();
+		marketText = marketText.GetComponent<Button> ();
+		optionsText = optionsText.GetComponent<Button> ();
+		exitText = exitText.GetComponent<Button> ();
+		quitMenu.enabled = false;
+
+	}
+	
+	public void ExitButtonPress()
+	{
+		quitMenu.enabled = true;
+		startText.enabled = false;
+		marketText.enabled = false;
+		optionsText.enabled = false;
+		exitText.enabled = false;
+	}
+
+	public void NoButtonPress()
+	{
+		quitMenu.enabled = false;
+		startText.enabled = true;
+		marketText.enabled = true;
+		optionsText.enabled = true;
+		exitText.enabled = true;
+	}
+
+	public void GoToSongSelect()
+	{
+		Application.LoadLevel (1);
+	}
+
+	public void GameExit()
+	{
+		Application.Quit ();
+	}
+}
