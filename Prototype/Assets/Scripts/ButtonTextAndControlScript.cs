c2bd2f68f9bc25686c4370d1e54630d2b6bf09e8
BloodOfTheWolf <kevin.v.hill@gmail.com>
2015-11-02 18:43:57 -0800

    [Song Selection Menu] - Implemented a state system for the menu, now press song button and then start

---
+++ Prototype/Assets/Scripts/ButtonTextAndControlScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonTextAndControlScript : MonoBehaviour {
+
+	//add public variable for each button we need in the song selection menu
+	public Button BackButton;
+	public Button TutorialButton;
+	public Button StartButton;
+	public int roomSelected = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+		TutorialButton.GetComponentInChildren<Text>().text = "Twinkle Twinkle Little Star (Tutorial)";
+		BackButton.GetComponentInChildren<Text>().text = "Back";
+		StartButton.GetComponentInChildren<Text>().text = "Start";
+	}
+
+	public void BackButtonPress()
+	{
+		Application.LoadLevel (0);
+	}
+
+	public void TutorialSelected()
+	{
+		roomSelected = 1;
+	}
+
+	public void GameStart()
+	{
+		if (roomSelected == 1) {
+			Application.LoadLevel(2);
+		}
+	}
+}
