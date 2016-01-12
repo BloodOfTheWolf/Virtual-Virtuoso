db3ee9a34d45d823d5ded564a09c210a2289451b
BloodOfTheWolf <kevin.v.hill@gmail.com>
2016-01-12 15:11:10 -0800

    [SFX Control] - Created a global controller for game sounds - Started a script for the controller, containing all SFX

---
+++ Prototype/Assets/Scripts/SFXControllerScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXControllerScript : MonoBehaviour {
+    
+    public AudioSource SFXSource;
+    public AudioClip ButtonPress;
+    public AudioClip CashRegister;
+    public AudioClip MassiveApplause;
+    public AudioClip MediumApplause;
+    public AudioClip MoneyGet;
+    public AudioClip OptionSwitch;
+    public AudioClip MultIncrease;
+    public AudioClip ScoreTally;
+    public AudioClip MultReset;
+    public AudioClip SmallApplause;
+    public AudioClip StarGet;
+    public AudioClip LargeApplause;
+    public AudioClip VolumeCtlEffect;
+    public AudioClip WrongNote;
+
+	// Use this for initialization
+	void Awake () {
+
+        SFXSource = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	
+	}
+}
