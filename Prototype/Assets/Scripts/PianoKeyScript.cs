6a0e7a3937eb3fef079d98e58e1baea0d4fbb54f
RobxMtz <betoescudero@hotmail.com>
2016-01-19 14:56:58 -0800

    [gameplay] - added multitouch.

--- Prototype/Assets/Scripts/PianoKeyScript.cs
+++ Prototype/Assets/Scripts/PianoKeyScript.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 [RequireComponent(typeof(AudioSource))]
 
@@ -12,6 +13,13 @@ public class PianoKeyScript : MonoBehaviour
 	private float NoteVol;
 	private GameObject Tutorial;
 	private bool Colored;
+
+	private List<GameObject> TouchList = new List<GameObject>();
+	private GameObject[] OldTouches;
+
+    //private Ray ray;
+    private RaycastHit RayHitInfo = new RaycastHit();
+   // public static int CurrTouch =0;
 
 		// Use this for initialization
 		void Awake () 
@@ -37,7 +45,62 @@ public class PianoKeyScript : MonoBehaviour
 		// Update is called once per frame
 		void Update ()
 		{
+            if(Input.touches.Length > 0)
+            {
+				OldTouches = new GameObject[TouchList.Count];
+				TouchList.CopyTo(OldTouches);
+				TouchList.Clear();
+				foreach(Touch touch in Input.touches)
+				{
+				Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
+				if(Physics.Raycast(ray , out RayHitInfo ))
+				{
+					GameObject recipient = RayHitInfo.transform.gameObject;
+					TouchList.Add(recipient);
+
+					if( touch.phase == TouchPhase.Began)
+					{
+						recipient.SendMessage("OnTouchDown");
+					}
+					if( touch.phase == TouchPhase.Ended)
+					{
+						recipient.SendMessage("OnTouchUp");
+					}
+				}
+
+
+
+				}
+
+				foreach(GameObject g in OldTouches)
+				{
+					if(!TouchList.Contains(g))
+					{
+					g.SendMessage("OnTouchUp");
+
+					}
+
+				}
+//                for(int i = 0; i < Input.touchCount ; i++)
+//                {
+//                    CurrTouch = i;
+//                    if(Input.GetTouch(i).phase == TouchPhase.Began)
+//                    {
+//                        ray = Camera.main.ScreenPointToRay( Input.GetTouch( i ).position );
+//                        if(Physics.Raycast(ray, out RayHitInfo))
+//                        {
+//							OnMouseDown();
+//                        }
+//                    }
+//
+//                    if(Input.GetTouch(i).phase == TouchPhase.Ended)
+//                    {
+//						OnMouseUp();
+//                    }
+//
+//                }
+            }
 			
 		}
 		
@@ -51,6 +114,20 @@ public class PianoKeyScript : MonoBehaviour
 	void OnMouseUp()
 	{
 
+		barnotes.SetActive(false);
+		LitNote.SetActive(false);
+	}
+
+	void OnTouchDown()
+	{
+		
+		audio.Play ();
+		barnotes.SetActive (true);
+		LitNote.SetActive (true);
+	}
+	void OnTouchUp()
+	{
+		
 		barnotes.SetActive(false);
 		LitNote.SetActive(false);
 	}
