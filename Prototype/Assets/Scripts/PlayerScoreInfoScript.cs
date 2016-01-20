45a3420f152f69d8c4e636ceeacceb32b51fc33b
BloodOfTheWolf <kevin.v.hill@gmail.com>
2016-01-19 17:48:57 -0800

    [Best Score] - Added scripts that interact to hold onto the player's best score, and checks new games to see if the score is exceeded

---
+++ Prototype/Assets/Scripts/PlayerScoreInfoScript.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerScoreInfoScript : MonoBehaviour {
+
+    public float MaryLambScore;
+    public float HotCrossScore;
+    public float MaryLambScoreHigh;
+    public float HotCrossScoreHigh;
+    private SheetNoteScript Info;
+    public string lastLevelPlayed;
+    
+    void Awake()
+    {
+        DontDestroyOnLoad( this.gameObject );
+    }
+    
+    // Use this for initialization
+	void Start () 
+    {
+        MaryLambScore = 0;
+        HotCrossScore = 0;
+        MaryLambScoreHigh = 0;
+        HotCrossScoreHigh = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //checks to see the player's highest score, and if the new score is higher than that
+        if( Application.loadedLevelName == "MaryLamb" )
+        {
+            lastLevelPlayed = "MaryLamb";
+            MaryLambScore = SheetNoteScript.Score;
+        }
+        
+        if (Application.loadedLevelName == "HotCrossBuns")
+        {
+            lastLevelPlayed = "HotCrossBuns";
+            HotCrossScore = SheetNoteScript.Score;
+        }
+	}
+}
---
+++ Prototype/Assets/Scripts/Results_BestScoreScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Results_BestScoreScript : MonoBehaviour 
+{
+    private PlayerScoreInfoScript Info;
+    float MaryLambScoreHigh;
+    float HotCrossScoreHigh;
+    GameObject PlayerInfo;
+    Text BestText;
+
+    void Awake()
+    {
+        DontDestroyOnLoad( this.gameObject );
+    }
+
+	// Use this for initialization
+	void Start () 
+    {
+        PlayerInfo = GameObject.Find( "PlayerInfoHolder" );
+        BestText = GetComponent<Text>();
+        BestText.text = ("blah");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        //check last level played, and then check the new score against the player's current highest
+        if( PlayerInfo.GetComponent<PlayerScoreInfoScript>().lastLevelPlayed == "MaryLamb" )
+        {
+            if( PlayerInfo.GetComponent<PlayerScoreInfoScript>().MaryLambScore > PlayerInfo.GetComponent<PlayerScoreInfoScript>().MaryLambScoreHigh )
+            {
+                PlayerInfo.GetComponent<PlayerScoreInfoScript>().MaryLambScoreHigh = PlayerInfo.GetComponent<PlayerScoreInfoScript>().MaryLambScore;
+            }
+            BestText.text = ("Best Score:" + PlayerInfo.GetComponent<PlayerScoreInfoScript>().MaryLambScoreHigh);
+        }
+
+        if( PlayerInfo.GetComponent<PlayerScoreInfoScript>().lastLevelPlayed == "HotCrossBuns" )
+        {
+            if( PlayerInfo.GetComponent<PlayerScoreInfoScript>().HotCrossScore > PlayerInfo.GetComponent<PlayerScoreInfoScript>().HotCrossScoreHigh )
+            {
+                PlayerInfo.GetComponent<PlayerScoreInfoScript>().HotCrossScoreHigh = PlayerInfo.GetComponent<PlayerScoreInfoScript>().HotCrossScore;
+            }
+            BestText.text = ("Best Score:" + PlayerInfo.GetComponent<PlayerScoreInfoScript>().HotCrossScoreHigh);
+        }
+
+	}
+}
