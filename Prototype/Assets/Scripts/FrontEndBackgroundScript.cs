1b7e70e38cd0560c2c7cd17f63b2a724dc19c633
Aaron Gilbert <marth8880@gmail.com>
2016-01-21 17:40:21 -0800

    [Fixed all notes in FurElise.unity & CanonInD.unity] [Fixed rhythm issues in Main.unity & MaryLamb.unity] [Implemented new 'Replace Selection' editor tool]

---
+++ Prototype/Assets/Scripts/FrontEndBackgroundScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrontEndBackgroundScript : MonoBehaviour
+{
+    public int MatIndex = 0;
+    public Vector2 ScrollRate = new Vector2( 1.0f, 0.0f );
+    public string TexName = "_MainTex";
+    public Renderer rend;
+    Vector2 uvOffset = Vector2.zero;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void LateUpdate()
+    {
+        uvOffset += (ScrollRate * Time.deltaTime);
+        if( rend.enabled )
+        {
+            rend.materials[MatIndex].SetTextureOffset( TexName, uvOffset );
+        }
+    }
+}
