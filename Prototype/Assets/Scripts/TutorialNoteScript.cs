de4bc290a1a14e64d69b9e1d1d31e0cc0b97caa1
Aaron Gilbert <marth8880@gmail.com>
2016-02-02 14:49:12 -0800

    [Finished bootstrap implementation in Boot.unity] [Fixed tutorial notes in Main.unity]

--- Prototype/Assets/Scripts/TutorialNoteScript.cs
+++ Prototype/Assets/Scripts/TutorialNoteScript.cs
@@ -3,24 +3,19 @@ using System.Collections;
 
 [RequireComponent(typeof(AudioSource))]
 
-public class TutorialNoteScript : MonoBehaviour {
+public class TutorialNoteScript : MonoBehaviour
+{
 	
 	 public GameObject NotetoLit;
 	 //private float NoteVol;
 
 	AudioSource sheetaudio;
-	// Use this for initialization
-	void Start () 
+	
+	void Start()
 	{
 		sheetaudio = GetComponent<AudioSource>();
 //		NoteVol = PianoVolumeScript.PKVolume;
 //		sheetaudio.volume = NoteVol; 
-	}
-	
-	// Update is called once per frame
-	void Update () 
-	{
-	
 	}
 
 	void OnTriggerEnter(Collider other)
@@ -29,7 +24,7 @@ public class TutorialNoteScript : MonoBehaviour {
 		{
 			NotetoLit.SetActive(true);
 			sheetaudio.Play();
-			print ("tutorial colided");
+			print("tutorial collided");
 		}
 	}
 
