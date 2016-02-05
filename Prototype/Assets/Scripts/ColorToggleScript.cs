cdd9b23f0cf6b9933fe62c839c6f6cd33b8c1aab
BloodOfTheWolf <kevin.v.hill@gmail.com>
2016-02-04 16:59:46 -0800

    [Player Feedback] - Modified script to change sprite for Color Toggle Mode - Added script to denote what song is selected through visuals

--- Prototype/Assets/Scripts/ColorToggleScript.cs
+++ Prototype/Assets/Scripts/ColorToggleScript.cs
@@ -7,11 +7,15 @@ public class ColorToggleScript : MonoBehaviour {
 
     public static bool Toggle;
     Text DifficultySetting;
+    Image ColorToggleSprite;
+    public Sprite ToggleOn;
+    public Sprite ToggleOff;
 	// Use this for initialization
 	void Start () 
     {
         DifficultySetting = this.gameObject.GetComponentInChildren<Text>();
         Toggle = false;
+        ColorToggleSprite = GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
@@ -25,13 +29,15 @@ public class ColorToggleScript : MonoBehaviour {
         if(Toggle == false)
         {
             Toggle = true;
-            DifficultySetting.text = "Color Toggle ON";
+            //DifficultySetting.text = "Color Toggle ON";
+            ColorToggleSprite.sprite = ToggleOn;
             print( Toggle );
         }
         else
         {
             Toggle = false;
-            DifficultySetting.text = "Color Toggle OFF";
+            //DifficultySetting.text = "Color Toggle OFF";
+            ColorToggleSprite.sprite = ToggleOff;
             print( Toggle );
         }
 
---
+++ Prototype/Assets/Scripts/SongSelectionSpriteScript.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SongSelectionSpriteScript : MonoBehaviour {
+
+    public Image TutorialSelected;
+    public Image HotCrossSelected;
+    public Image MaryLambSelected;
+    public Image FurEliseSelected;
+    public Image CanonSelected;
+    public Image EntertainerSelected;
+
+	void Start () 
+    {
+        TutorialSelected.enabled = true;
+        HotCrossSelected.enabled = false;
+        MaryLambSelected.enabled = false;
+        FurEliseSelected.enabled = false;
+        CanonSelected.enabled = false;
+        EntertainerSelected.enabled = false;
+	}
+	
+	public void TutorialActive () 
+    {
+        TutorialSelected.enabled = true;
+        HotCrossSelected.enabled = false;
+        MaryLambSelected.enabled = false;
+        FurEliseSelected.enabled = false;
+        CanonSelected.enabled = false;
+        EntertainerSelected.enabled = false;
+	}
+
+    public void HotCrossActive()
+    {
+        TutorialSelected.enabled = false;
+        HotCrossSelected.enabled = true;
+        MaryLambSelected.enabled = false;
+        FurEliseSelected.enabled = false;
+        CanonSelected.enabled = false;
+        EntertainerSelected.enabled = false;
+    }
+
+    public void MaryLambActive()
+    {
+        TutorialSelected.enabled = false;
+        HotCrossSelected.enabled = false;
+        MaryLambSelected.enabled = true;
+        FurEliseSelected.enabled = false;
+        CanonSelected.enabled = false;
+        EntertainerSelected.enabled = false;
+    }
+
+    public void FurEliseActive()
+    {
+        TutorialSelected.enabled = false;
+        HotCrossSelected.enabled = false;
+        MaryLambSelected.enabled = false;
+        FurEliseSelected.enabled = true;
+        CanonSelected.enabled = false;
+        EntertainerSelected.enabled = false;
+    }
+
+    public void CanoninDSelected()
+    {
+        TutorialSelected.enabled = false;
+        HotCrossSelected.enabled = false;
+        MaryLambSelected.enabled = false;
+        FurEliseSelected.enabled = false;
+        CanonSelected.enabled = true;
+        EntertainerSelected.enabled = false;
+    }
+
+    public void TheEntertainerSelected()
+    {
+        TutorialSelected.enabled = false;
+        HotCrossSelected.enabled = false;
+        MaryLambSelected.enabled = false;
+        FurEliseSelected.enabled = false;
+        CanonSelected.enabled = false;
+        EntertainerSelected.enabled = true;
+    }
+}
