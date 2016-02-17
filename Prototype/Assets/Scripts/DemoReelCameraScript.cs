45689cdcda45173e6908d27e63345617b8acbd3b
BloodOfTheWolf <kevin.v.hill@gmail.com>
2016-02-16 16:51:33 -0800

    [Main Game] - Options menu implemented - All graphics and functionality implemented (except Volume Settings)

---
+++ Prototype/Assets/Scripts/DemoReelCameraScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoReelCameraScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	Screen.SetResolution((int)Screen.width, (int)Screen.height, true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Prototype/Assets/Scripts/InGameOptionsScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InGameOptionsScript : MonoBehaviour 
+{
+    public Button OptionsButton;
+    public Canvas OptionsTab;
+	// Use this for initialization
+	void Start () 
+    {
+        OptionsTab.enabled = false;
+	}
+	
+	// Update is called once per frame
+	public void OptionsPressed () 
+    {
+        if (OptionsTab.enabled == false)
+        {
+            OptionsButton.interactable = false;
+            Time.timeScale = 0.0f;
+            OptionsTab.enabled = true;
+        }
+	}
+
+    public void ResumePressed()
+    {
+        OptionsTab.enabled = false;
+        Time.timeScale = 1.0f;
+        OptionsButton.interactable = true;
+    }
+
+    public void RetryPressed()
+    {
+        Application.LoadLevel( Application.loadedLevelName );
+    }
+
+    public void SettingsPressed()
+    {
+
+    }
+
+    public void QuitPressed()
+    {
+        Application.LoadLevel( 5 );
+    }
+}
