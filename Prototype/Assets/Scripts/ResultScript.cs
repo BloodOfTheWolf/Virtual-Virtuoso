e3749ae311e8f1dbff727693222581d9aca373a7
RobxMtz <betoescudero@hotmail.com>
2016-02-23 15:13:07 -0800

    [results screen] - fixed infinite loop.

--- Prototype/Assets/Scripts/ResultScript.cs
+++ Prototype/Assets/Scripts/ResultScript.cs
@@ -7,39 +7,49 @@ public class ResultScript : MonoBehaviour
 	private SheetNoteScript Results;
 	private int Score;
     private int ScoreInitializer;
+    Text ScoreText;
 	
 	void Start() 
 	{
         ScoreInitializer = 0;
 		Score = SheetNoteScript.Score;
+
+        ScoreText = GetComponent<Text>();
+        
 	}
 
     void Update()
     {
-        Text ScoreText = GetComponent<Text>();
-        ScoreText.text = ScoreInitializer.ToString();
-        if (ScoreInitializer != Score)
+
+        while( ScoreInitializer != Score )
         {
-            if( (Score - ScoreInitializer) > 1000 )
-            {
-                ScoreInitializer += 75;
-            }
-            else if( (Score - ScoreInitializer) <= 1000 )
-            {
-                ScoreInitializer += 50;
-            }
-            else if( (Score - ScoreInitializer) <= 500 )
-            {
-                ScoreInitializer += 25;
-            }
-            else if( (Score - ScoreInitializer) <= 100 )
-            {
-                ScoreInitializer += 10;
-            }
-            else if( (Score - ScoreInitializer) < 100 )
-            {
-                ScoreInitializer += 1;
-            }
+            ScoreInitializer += 2;
+            ScoreText.text = ScoreInitializer.ToString();
+
         }
+
+        //if (ScoreInitializer != Score)
+        //{
+        //    if( (Score - ScoreInitializer) > 1000 )
+        //    {
+        //        ScoreInitializer += 75;
+        //    }
+        //    else if( (Score - ScoreInitializer) <= 1000 )
+        //    {
+        //        ScoreInitializer += 50;
+        //    }
+        //    else if( (Score - ScoreInitializer) <= 500 )
+        //    {
+        //        ScoreInitializer += 25;
+        //    }
+        //    else if( (Score - ScoreInitializer) <= 100 )
+        //    {
+        //        ScoreInitializer += 10;
+        //    }
+        //    else if( (Score - ScoreInitializer) < 100 )
+        //    {
+        //        ScoreInitializer += 1;
+        //    }
+        //}
     }
 }
