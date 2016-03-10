267a95c0cc9dbc1df06903ba84d9a28fbe357b1a
RobxMtz <betoescudero@hotmail.com>
2016-03-10 05:19:07 -0800

    [bgm] - started setting background music properly.

---
+++ Prototype/Assets/Scripts/BackgroundSongToggleScript.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSongToggleScript : MonoBehaviour {
+
+    public static bool Toggle;
+    public static int RefNum;
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+        
+	
+	}
+	
+	public void CheckToggle()
+    {
+        
+       
+    }
+
+    public void ChangeBackgroundSong( int i )
+    {
+        switch( i )
+        {
+        case 1:
+        if( SongholderScript.BGSong.clip.name != "02 Piano" || SongholderScript.IsCreated == true  )
+        {
+            SongholderScript.BGSong.clip = Resources.Load<AudioClip>( "Songs/02 Piano" );
+                print( "entered is playing" );
+                SongholderScript.BGSong.Play();
+            print( "entered changebgs case 1" );
+            //if( SongholderScript.ItsPlaying == false )
+            //{
+            //}
+        SongholderScript.CurrentSong = 1;
+        }
+        break;
+        case 2:
+        if( SongholderScript.BGSong.clip.name != "mus_etude_32khz" || SongholderScript.IsCreated == true )
+        {
+            SongholderScript.BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_etude_32khz" );
+            print( "entered changebgs case 2" );
+                SongholderScript.BGSong.Play();
+            //if( SongholderScript.ItsPlaying == false )
+            //{
+            //    print( "entered is playing" );
+            //}
+        SongholderScript.CurrentSong = 2;
+        }
+        break;
+        case 3:
+        if( SongholderScript.BGSong.clip.name != "mus_sonata_32khz" || SongholderScript.IsCreated == true )
+        {
+            SongholderScript.BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_sonata_32khz" );
+                print( "entered is playing" );
+                SongholderScript.BGSong.Play();
+            print( "entered changebgs case 3" );
+            //if( SongholderScript.ItsPlaying == false )
+            //{
+            //}
+        SongholderScript.CurrentSong = 3;
+        }
+        break;
+        case 4:
+        if( SongholderScript.BGSong.clip.name != "mus_jesu_32khz" || SongholderScript.IsCreated == true )
+        {
+            SongholderScript.BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_jesu_32khz" );
+                print( "entered is playing" );
+                SongholderScript.BGSong.Play();
+            print( "entered changebgs case 4" );
+            //if( SongholderScript.ItsPlaying == false )
+            //{
+            //}
+        SongholderScript.CurrentSong = 4;
+        }
+
+        break;
+        case 5:
+        if( SongholderScript.BGSong.clip.name != "mus_rondo_32khz" || SongholderScript.IsCreated == true )
+        {
+            SongholderScript.BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_rondo_32khz" );
+                print( "entered is playing" );
+                SongholderScript.BGSong.Play();
+            print( "entered changebgs case 5" );
+            //if( SongholderScript.ItsPlaying == false )
+            //{
+            //}
+        SongholderScript.CurrentSong = 5;
+        }
+        break;
+
+        }
+    }
+}
--- Prototype/Assets/Scripts/SongholderScript.cs
+++ Prototype/Assets/Scripts/SongholderScript.cs
@@ -10,6 +10,10 @@ public class SongholderScript : MonoBehaviour {
 	public static float BGMVolume;
 	public static AudioSource BGSong;
 	Slider BGMSlider;
+    public static int CurrentSong;
+    public static bool FindToggle;
+    public static bool ItsPlaying;
+
 
 	//public static List<AudioSource> songs = new List<AudioSource>();
 
@@ -17,16 +21,26 @@ public class SongholderScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-
+        
 		//if (ListOfNames == ListofAduiSources[i].name)
 		//play me
-
+        print(" its playing value"  + ItsPlaying );
 		BGMSlider = GameObject.Find("BGMSlider").GetComponent<Slider> ();
 		if (!IsCreated) 
 		{
 			IsCreated = true;
 			BGSong = GetComponent<AudioSource>();
-			BGSong.Play ();
+            if(CurrentSong == 0)
+            {
+                print( "entered start is created current song = 0" );
+                ChangeBackgroundSong( 1 );
+            }
+            else
+            {
+                print( "entered start is created else" );
+                ChangeBackgroundSong( CurrentSong );
+
+            }
 			DontDestroyOnLoad(this.gameObject) ;
 			BGMVolume = BGMSlider.value;
             print( "entered !iscreated" );
@@ -38,10 +52,108 @@ public class SongholderScript : MonoBehaviour {
 			Destroy (this.gameObject);
            // BGSong.volume = BGMSliderScript.BGVolumeVal;
             print( "entered else of !iscreated" );
-
+            //ToggleSongIcon( CurrentSong );
 		}
-
+        
         
 	}
+
+    public void ChangeBackgroundSong(int i)
+    {
+        switch(i)
+        {
+        case 1:
+        if( BGSong.clip.name != "02 Piano" || IsCreated == true)
+        {
+            print( "entered changebgs case 1" );
+            BGSong.clip = Resources.Load<AudioClip>( "Songs/02 Piano" );
+            if( BGSong.isPlaying == false )
+            {
+                print( "entered is playing" );
+                BGSong.Play();
+                ItsPlaying = true;
+            }
+        }
+        CurrentSong = 1;
+        break;
+        case 2:
+        if( BGSong.clip.name != "mus_etude_32khz"  )
+        {
+            print( "entered changebgs case 2" );
+            BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_etude_32khz" );
+            if( BGSong.isPlaying == false )
+            {
+                print( "entered is playing" );
+                BGSong.Play();
+            }
+        }
+        CurrentSong = 2;
+        break;
+        case 3:
+        if( BGSong.clip.name != "mus_sonata_32khz"  )
+        {
+            print( "entered changebgs case 3" );
+            BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_sonata_32khz" );
+            if( BGSong.isPlaying == false )
+            {
+                print( "entered is playing" );
+                BGSong.Play();
+            }
+        }
+        CurrentSong = 3;
+        break;
+        case 4:
+        if( BGSong.clip.name != "mus_jesu_32khz"  )
+        {
+            print( "entered changebgs case 4" );
+            BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_jesu_32khz" );
+            if( BGSong.isPlaying == false )
+            {
+                print( "entered is playing" );
+                BGSong.Play();
+            }
+        }
+
+        CurrentSong = 4;
+        break;
+        case 5:
+        if( BGSong.clip.name != "mus_rondo_32khz"  )
+        {
+            print( "entered changebgs case 5" );
+            BGSong.clip = Resources.Load<AudioClip>( "Songs/mus_rondo_32khz" );
+            if( BGSong.isPlaying == false )
+            {
+                print( "entered is playing" );
+                BGSong.Play();
+            }
+        }
+        CurrentSong = 5;
+        break;
+
+        }
+    }
+
+    public void ToggleSongIcon(int i)
+    {
+        switch(i)
+        {
+        case 1:
+        FindToggle = GameObject.Find( "FirstSongToggle" ).GetComponent<Toggle>().isOn = true;
+        break;
+        case 2:
+        FindToggle = GameObject.Find( "EtudeToggle" ).GetComponent<Toggle>().isOn = true;
+        break;
+        case 3:
+        FindToggle = GameObject.Find( "SonataToggle" ).GetComponent<Toggle>().isOn = true;
+        break;
+        case 4:
+        FindToggle = GameObject.Find( "JesuToggle" ).GetComponent<Toggle>().isOn = true;
+        break;
+        case 5:
+        FindToggle = GameObject.Find( "RondoToggle" ).GetComponent<Toggle>().isOn = true;
+        break;
+
+        }
+    }
 
 }
