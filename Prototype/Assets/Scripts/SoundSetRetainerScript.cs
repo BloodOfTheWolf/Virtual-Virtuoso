9cc9993f78d012134f0e0ce2c64be18c331fa17c
BloodOfTheWolf <kevin.v.hill@gmail.com>
2016-03-10 03:39:54 -0800

    [Main Menu and Results] - Realigned some assets in the Results screen to improve appearance - Started script to hold soundset info

---
+++ Prototype/Assets/Scripts/SoundSetRetainerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SoundSetRetainerScript : MonoBehaviour 
+{
+    public Image PianoSelect;
+
+
+	void Start () 
+    {
+
+	}
+	
+
+	void Update () 
+    {
+	
+	}
+}
