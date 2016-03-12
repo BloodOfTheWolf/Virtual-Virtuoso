891b794a73620c9ff1e7be9a87e0a9423716855e
Aaron Gilbert <marth8880@gmail.com>
2016-03-11 16:48:08 -0800

    [Finished fixing main-game options menu] - also updated project settings

--- Prototype/Assets/Scripts/InGameOptionsScript.cs
+++ Prototype/Assets/Scripts/InGameOptionsScript.cs
@@ -5,30 +5,36 @@ using UnityEngine.UI;
 public class InGameOptionsScript : MonoBehaviour 
 {
     public Button OptionsButton;
-    public Canvas OptionsTab;
-    public GameObject OptionsSettingTab;
+    public GameObject OptionsPanel;
+    public GameObject OptionsSettingsPanel;
     public Button OptionsSettingsConfirm;
     public Button OptionsSettingsCancel;
 
+    public GameObject OptionsBackgroundPanel;
+    public Button OptionsBackgroundButton;
+    public GameObject OptionsSettingsBackgroundPanel;
+    public Button OptionsSettingsBackgroundButton;
+
+
 	void Start()
     {
-        OptionsTab.enabled = false;
-        OptionsSettingTab.SetActive(false);
+        OptionsPanel.SetActive(false);
+        OptionsSettingsPanel.SetActive(false);
 	}
 	
 	public void OptionsPressed() 
     {
-        if (OptionsTab.enabled == false)
+        if (OptionsPanel.activeSelf == false)
         {
             OptionsButton.interactable = false;
             Time.timeScale = 0.0f;
-            OptionsTab.enabled = true;
+            OptionsPanel.SetActive(true);
         }
 	}
 
     public void ResumePressed()
     {
-        OptionsTab.enabled = false;
+        OptionsPanel.SetActive(false);
         Time.timeScale = 1.0f;
         OptionsButton.interactable = true;
     }
@@ -41,27 +47,26 @@ public class InGameOptionsScript : MonoBehaviour
 
     public void SettingsPressed()
     {
-        if( OptionsSettingTab.activeSelf == false )
+        if( OptionsSettingsPanel.activeSelf == false )
         {
             //OptionsButton.interactable = false;
             //Time.timeScale = 0.0f;
-            OptionsSettingTab.SetActive( true );
-            
+            OptionsSettingsPanel.SetActive( true );
+            OptionsBackgroundButton.interactable = false;
         }
     }
 
     public void QuitPressed()
     {
         Time.timeScale = 1.0f;
-        Application.LoadLevel( 5 );
+        Application.LoadLevel( "SongSelectionMenu" );
     }
 
     public void SettingsConfirmPressed()
     {
-        OptionsSettingTab.SetActive(false);
+        OptionsSettingsPanel.SetActive(false);
         //Time.timeScale = 1.0f;
         //OptionsButton.interactable = true;
+        OptionsBackgroundButton.interactable = true;
     }
-
-    
 }
